216ebc0647afb9370297064f06942210be958827
Carlo Occhiena <57464184+carloocchiena@users.noreply.github.com>
2021-12-01 23:12:54 +0100

    Add files via upload

--- main_app.py
+++ main_app.py
@@ -1,36 +1,56 @@
-from flask import Flask, request, render_template
-
 from datetime import date
+
+from flask import Flask, request, render_template
 
 from insert_user import insert_user
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-#routing error page 404
+# render error page 404
 @app.errorhandler(404)
 def page_not_found(e):
     return "<h1>OOPS</h1> <p> Page not found </p>", 404
 
-#render main page
+# render admin page # ok siamo arriva qui, a validare la password
+@app.route('/admin', methods = ["GET", "POST"])
+def admin():
+    
+    if request.method == "POST":
+        psw = request.form['password']
+        
+    """
+    def api_all():
+    conn = sqlite3.connect(DB)
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    all_album = cur.execute("SELECT * FROM album;").fetchall()
+    
+    return jsonify(all_album)
+    """
+    return render_template("admin.html")
+
+# render main page
 @app.route('/', methods = ["GET", "POST"])
 def input_page():
+    
     errors = ""
     today = date.today()
     
     if request.method == "POST":
-        user = None
         user = request.form['user']
       
-        if user is not None:
+        if user != "":
             try:
                 insert_user(user)
-                return render_template("results.html", user=user, today_date=today)     
+                return render_template("results.html", user=user, today_date=today)
             
             except Exception as e:
-                #errors = "🐸 The anti-bug frog has caught something! Can you try? Sorry, it's still a beta version!"
-                errors = e   
+                errors = f"[!] Errors found: {e}"
+        else:
+            errors = "You must insert a username first"
 
     return render_template("main.html", today=today, errors=errors)
 
-app.run(use_reloader=False)
+if __name__ == '__main__':
+    app.run(use_reloader=False)
